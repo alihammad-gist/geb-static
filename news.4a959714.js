91c9b2f2b908fbcb76f76c16a974ffc293d243ac
Ali Hammad Shah <syedalihammadshah@googlemail.com>
2019-11-12 13:57:12 +0500

    made team member name and title bold

--- news.4a959714.js
+++ news.4a959714.js
@@ -32099,7 +32099,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36963" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35791" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
