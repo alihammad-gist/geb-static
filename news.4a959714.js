7423983c8102f41ba7c7cf2eb94b590cea8e555c
Ali Hammad Shah <syedalihammadshah@googlemail.com>
2019-11-22 10:45:28 +0500

    workin ..

--- news.4a959714.js
+++ news.4a959714.js
@@ -32099,7 +32099,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42211" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35339" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
