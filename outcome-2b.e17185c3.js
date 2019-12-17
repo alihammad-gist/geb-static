ab7298035bcadbd2688302c28d22b0a168340efe
Ali Hammad <syedalihammadshah@gmail.com>
2019-12-17 06:33:16 +0500

    decreased navbar font, change tagline bg blur to black from $grey

--- outcome-2b.e17185c3.js
+++ outcome-2b.e17185c3.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39113" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39881" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
